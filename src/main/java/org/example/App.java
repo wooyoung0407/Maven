c077d2219f9c548d675fc2e844f34ff44af61376
wooyoung0407 <wooyoung0407@naver.com>
2023-04-17 13:29:05 +0900

    Maven-2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,6 +13,47 @@ public class App {
         Scanner sc = Container.scanner;
         List<Article> articles = new ArrayList<>();
         int articleLastId = 0;
+
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+
+            String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeBehavior=convertToNull";
+
+            conn = DriverManager.getConnection(url, "root", "");
+
+            String sql = "INSERT INTO article";
+            sql += " SET regDate = NOW()";
+            sql += ", updateDate = NOW()";
+            sql += ", title = 'test3'";
+            sql += ", content = 'test4'";
+
+            pstmt = conn.prepareStatement(sql);
+
+            int affectRows = pstmt.executeUpdate();
+
+            System.out.println("affectRows = " + affectRows);
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("드라이버 로딩 실패");
+        }
+        catch(SQLException e){
+            System.out.println("에러: " + e);
+        }
+        finally{
+            try{
+                if( conn != null && !conn.isClosed()){
+                    conn.close();
+                }
+            }
+            catch( SQLException e){
+                e.printStackTrace();
+            }
+        }
 
         while (true) {
             System.out.printf("명령어)");
